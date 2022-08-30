55915c46733ca7dbcd1395a4256b2c1b1be03885
Diego Machín <diego@acidtango.com>
2022-08-30 10:51:43 +0100

    feat: integration of postgres on deployed

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ Main entrypoint
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from src.config.db import DATABASE_CONFIG
+from src.config.db import DATABASE_URL_CONECTION
 # pylint: disable=import-outside-toplevel
 
 db = SQLAlchemy()
@@ -15,13 +15,10 @@ def create_app():
     create and configure the app
     """
     app = Flask(__name__, instance_relative_config=True)
-    print(DATABASE_CONFIG)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI=(
-            "postgresql://{host}:{port}/{database_name}?user={username}&password={password}"
-            .format(**DATABASE_CONFIG)
-        )
+        SQLALCHEMY_DATABASE_URI=DATABASE_URL_CONECTION
     )
     db.init_app(app)
 
