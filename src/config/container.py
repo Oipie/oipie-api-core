cd05f1f5790c45f79c567221157c60a35244bb57
Jaume Moreno <morenocantoj@gmail.com>
2022-09-21 16:59:20 +0200

    feat: make tokenizer a provider and inject it to users login use case

--- src/config/container.py
+++ src/config/container.py
@@ -3,6 +3,7 @@
 """
 from dependency_injector import containers, providers
 from src.config.database import Database
+from src.config.jwt import JWT_SECRET_KEY
 from src.core.recipes.infrastructure.recipes_repository_sqlalchemy import (
     RecipesRepositorySQLAlchemy,
 )
@@ -10,6 +11,7 @@ from src.core.users.infrastructure.user_repository_sqlalchemy import UsersReposi
 from src.core.users.application.users_registerer import UsersRegisterer
 from src.core.users.application.users_login import UsersLogin
 from src.shared.services.password.argon2_password import Argon2Password
+from src.shared.services.tokenizer.jwt_tokenizer import JwtTokenizer
 
 
 class Container(containers.DeclarativeContainer):
@@ -20,6 +22,8 @@ class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(packages=["..api", "src.api.auth"])
 
     db = providers.Singleton(Database)
+    tokenizer = JwtTokenizer(JWT_SECRET_KEY)
+
     # pylint: disable=no-member
     recipes_repository = providers.Singleton(
         RecipesRepositorySQLAlchemy, session_factory=db.provided.session
@@ -32,7 +36,10 @@ class Container(containers.DeclarativeContainer):
         UsersRegisterer, users_repository=users_repository, password_hasher=password_hasher
     )
     users_login = providers.Singleton(
-        UsersLogin, users_repository=users_repository, password_hasher=password_hasher
+        UsersLogin,
+        users_repository=users_repository,
+        password_hasher=password_hasher,
+        tokenizer=tokenizer,
     )
 
     # pylint: enable=no-member
